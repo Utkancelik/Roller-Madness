dfab30a760460da9f56108e611c1cf96e308d3fb
Utkan <utkancelik@hacettepe.edu.tr>
2023-01-30 13:28:20 +0300

    After game ends or finished, all objects in the world destroyes itself.

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -8,8 +8,14 @@ public class TimeManager : MonoBehaviour
     [SerializeField] private float levelFinishTime = 15.0f;
     [SerializeField] private Text timeText;
     [SerializeField] private GameObject winPanel, losePanel;
+    [SerializeField] private List<GameObject> destroyAfterGame = new List<GameObject>();
+
     public bool gameFinished = false;
     public bool gameOver = false;
+    private void Start()
+    {
+        
+    }
     private void Update()
     {
         if (!gameFinished && !gameOver)
@@ -22,13 +28,34 @@ public class TimeManager : MonoBehaviour
             gameFinished = true;
             winPanel.SetActive(true);
             losePanel.SetActive(false);
+
+            UpdateObjectsList("Objects");
+            UpdateObjectsList("Enemy");
+
+            foreach (GameObject entity in destroyAfterGame)
+            {
+                Destroy(entity);
+            }
         }
 
         if (gameOver)
         {
             losePanel.SetActive(true);
             winPanel.SetActive(false);
+
+            UpdateObjectsList("Objects");
+            UpdateObjectsList("Enemy");
+
+            foreach (GameObject entity in destroyAfterGame)
+            {
+                Destroy(entity);
+            }
         }
+    }
+
+    private void UpdateObjectsList(string tag)
+    {
+        destroyAfterGame.AddRange(GameObject.FindGameObjectsWithTag(tag));
     }
 
     private void UpdateTimer()
